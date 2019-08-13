15933c3802b5a9bda8cadaad43925be4f8e40fba
ATHeart <b.angelthink@gmail.com>
2019-08-13 20:21:51 +0900

    Timer
    - Added Start Button
      - increase 0.1 seconds on press
    - Added Stop Button
      - stop increase
    - Added reset time
      - reset seconds

--- timer/timer/ViewController.swift
+++ timer/timer/ViewController.swift
@@ -9,12 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblTimer: UILabel!
+    @IBOutlet var btnStart: UIButton!
+    @IBOutlet var btnReset: UIButton!
+    
+    var timer: Timer? = nil
+    var status = 0
+    
+    let timerSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 0.1
+    var count = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        btnStart.setTitle("start", for: .normal)
+        btnReset.setTitle("reset", for: .normal)
     }
-
-
+    @IBAction func btnStart(_ sender: UIButton!) {
+        if(status == 0) {
+            timer = Timer.scheduledTimer(timeInterval: interval, target: self, selector: timerSelector, userInfo: nil, repeats: true)
+            status = 1
+            setButton()
+        } else {
+            timer?.invalidate()
+            timer = nil
+            status = 0
+            setButton()
+        }
+    }
+    
+    @IBAction func btnReset(_ sender: UIButton!) {
+        timer?.invalidate()
+        timer = nil
+        
+        count = 0
+        status = 0
+        setButton()
+        setTimerTxt()
+    }
+    
+    
+    @objc func updateTime() {
+        count += interval
+        setTimerTxt()
+    }
+    
+    func setTimerTxt() {
+        lblTimer.text = String(format: "%.1f", count)
+    }
+    
+    func setButton() {
+        if(status == 0) {
+            btnStart.setTitle("start", for: .normal)
+        } else {
+            btnStart.setTitle("stop", for: .normal)
+        }
+    }
 }
 
