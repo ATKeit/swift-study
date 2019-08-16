f9cbf0a6024241d838d36f1d243a05fa9c38ff76
ATHeart <b.angelthink@gmail.com>
2019-08-16 16:24:30 +0900

    Added AutoLayout

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by B.Cheolu on 16/08/2019.
+//  Copyright © 2019 B.Cheolu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
